60cbf31bc0d24d3eb8e5466fddb0b9a0f9424127
Sui Qingyu <sqybi@126.com>
2012-08-20 10:07:23 +0800

    the client rendering function is almost finished (without bricks).

--- client.js
+++ client.js
@@ -4,15 +4,16 @@
 
 var ctxWidth          = ctx[0].canvas.width;  // width of the whole canvas
 var ctxHeight         = ctx[0].canvas.height; // height of the whole canvas
-var gameRegionSize    = 600;                  // the side length of game region
-var gameRegionGapSize = 10;                   // size of the gap between game region and canvas (to place the boards)
-var boardThickness    = 8;                    // thickness of the boards
+var gameRegionSize    = 600;                  // the side length of game region, NEVER change it
+var gameRegionGapSize = 10;                   // size of the gap between game region and canvas (to place the boards), NEVER change it
+var boardThickness    = 9;                    // thickness of the boards
+var ballSize          = 7;                   // radius of the ball
 
 // status of the game
 //   0: game not started yet
 //   1: game is running
 //   2: you are dead!
-var gameStatus = 0;
+var gameStatus = 2;
 
 var ball = {
     x: gameRegionSize / 2,
@@ -47,6 +48,7 @@ var boards = [
 
 //
 // action when key pressed
+// currently no action -- control the game with mouse
 //
 // use this function to get all the key strokes, the code can be the following:
 // right, left, up, down, a, b, c, ... , z, 1, 2, ... , 9,  (a space for space bar)
@@ -66,6 +68,10 @@ function KeyPress(code) {
             // do nothing
     }
 }
+
+//
+// a function needed here to control the board with mouse
+//
 
 //
 // action when a message is coming
@@ -105,6 +111,81 @@ function UpdateBoardInformation(id, position, size) {
 }
 
 //
+// Draw the game area
+//
+
+function DrawGameArea(ctx) {
+    // draw all the boards by anticlockwise order
+    var boardLeftBoundary, boardRightBoundary;
+
+    boardLeftBoundary = boards[0].position - boards[0].size;
+    boardRightBoundary = boards[0].position + boards[0].size;
+    if (boardLeftBoundary < 0) {
+        boardLeftBoundary = 0;
+    }
+    if (boardRightBoundary >= gameRegionSize) {
+        boardRightBoundary = gameRegionSize - 1;
+    }
+    ctx.fillStyle = "red";
+    ctx.fillRect(boardLeftBoundary + gameRegionGapSize + 2, gameRegionSize + gameRegionGapSize + 3,
+                 boardRightBoundary - boardLeftBoundary + 1, boardThickness);
+
+    boardLeftBoundary = gameRegionSize - 1 - boards[1].position + boards[1].size;
+    boardRightBoundary = gameRegionSize - 1 - boards[1].position - boards[1].size;
+    if (boardLeftBoundary >= gameRegionSize) {
+        boardLeftBoundary = gameRegionSize - 1;
+    }
+    if (boardRightBoundary < 0) {
+        boardRightBoundary = 0;
+    }
+    ctx.fillStyle = "blue";
+    ctx.fillRect(gameRegionSize + gameRegionGapSize + 3, boardRightBoundary + gameRegionGapSize + 2,
+                 boardThickness, boardLeftBoundary - boardRightBoundary + 1);
+
+    boardLeftBoundary = gameRegionSize - 1 - boards[2].position + boards[2].size;
+    boardRightBoundary = gameRegionSize - 1 - boards[2].position - boards[2].size;
+    if (boardLeftBoundary >= gameRegionSize) {
+        boardLeftBoundary = gameRegionSize - 1;
+    }
+    if (boardRightBoundary < 0) {
+        boardRightBoundary = 0;
+    }
+    ctx.fillStyle = "green";
+    ctx.fillRect(boardRightBoundary + gameRegionGapSize + 2, gameRegionGapSize - boardThickness + 1,
+                 boardLeftBoundary - boardRightBoundary + 1, boardThickness);
+
+    boardLeftBoundary = boards[3].position - boards[3].size;
+    boardRightBoundary = boards[3].position + boards[3].size;
+    if (boardLeftBoundary < 0) {
+        boardLeftBoundary = 0;
+    }
+    if (boardRightBoundary >= gameRegionSize) {
+        boardRightBoundary = gameRegionSize - 1;
+    }
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(gameRegionGapSize - boardThickness + 1, boardLeftBoundary + gameRegionGapSize + 2,
+                 boardThickness, boardRightBoundary - boardLeftBoundary + 1);
+
+    // draw the bricks (no bricks yet)
+
+    // draw the ball
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(gameRegionGapSize + 2 + ball.x, gameRegionGapSize + 2 + ball.y,
+            ballSize, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+//
+// Draw the information area
+//
+
+function DrawInfoArea(ctx) {
+
+}
+
+//
 // request a new frame
 //
 
@@ -124,7 +205,7 @@ var NewFrame = function() {
     else if (window.oRequestAnimationFrame) {
         window.oRequestAnimationFrame(RenderingLoop);
     }
-    else {
+    else { // No requestAnimationFrame feature
         NewFrame = function() {};
         window.setInterval(RenderingLoop, 16.7);
     }
@@ -151,8 +232,8 @@ var RenderingLoop = function() {
     backgroundCtx.strokeRect(0, 0, ctxWidth, ctxHeight);
     backgroundCtx.strokeRect(gameRegionGapSize + 1, gameRegionGapSize + 1, gameRegionSize + 2, gameRegionSize + 2);
     backgroundCtx.beginPath();
-    backgroundCtx.moveTo(gameRegionSize + gameRegionGapSize * 2 + 2, 0);
-    backgroundCtx.lineTo(gameRegionSize + gameRegionGapSize * 2 + 2, ctxHeight);
+    backgroundCtx.moveTo(gameRegionSize + gameRegionGapSize * 2 + 3, 0);
+    backgroundCtx.lineTo(gameRegionSize + gameRegionGapSize * 2 + 3, ctxHeight);
     backgroundCtx.stroke();
 
     switch (gameStatus) {
@@ -161,19 +242,29 @@ var RenderingLoop = function() {
         case 0:
             backgroundCtx.textBaseline = "middle";
             backgroundCtx.textAlign = "center";
+            backgroundCtx.fillStyle = "black";
             backgroundCtx.fillText("Waiting for other participants...", gameRegionGapSize + gameRegionSize / 2 + 2, gameRegionGapSize + gameRegionSize / 2 + 2);
+            DrawInfoArea(backgroundCtx);
 
             break;
 
         // game is running
         case 1:
-            // draw boards
-
+            DrawGameArea(backgroundCtx);
+            DrawInfoArea(backgroundCtx);
 
             break;
 
         // you are dead
         case 2:
+            DrawGameArea(backgroundCtx);
+
+            backgroundCtx.textBaseline = "middle";
+            backgroundCtx.textAlign = "center";
+            backgroundCtx.fillStyle = "red";
+            backgroundCtx.fillText("You are dead...", gameRegionGapSize + gameRegionSize / 2 + 2, gameRegionGapSize + gameRegionSize / 2 + 2);
+
+            DrawInfoArea(backgroundCtx);
 
             break;
 
